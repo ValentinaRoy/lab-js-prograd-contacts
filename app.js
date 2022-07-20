77a9ede684af06d0c16db87bf6a6a9bea5184b98
Valentina Roy <tinar478612@gmail.com>
2022-07-20 20:23:32 +0530

    msg

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-const btn = document.querySelector("#btnGet");
-const msg = document.querySelector("#message");
-function getData () {
+const btn = document.querySelector('#btnGet');
+const msg = document.querySelector('#message');
+function getData() {
   const promise = new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.open("GET", "https://jsonplaceholder.typicode.com/users");
-    request.onload = () => {
-      if (request.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    };
-    request.onerror = () => {
-      reject(Error("Error fetching data"));
-    };
-    request.send();
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((data) => resolve(data))
+      .catch((err) => reject(err));
+
   });
   promise.then(
-    (dataz) => {
-      console.log("Promise Executed");
-      const result = JSON.parse(dataz);
-      var player = "<h2>Lists of Users</h2>";
-      result.forEach(function (user) {
+    (data) => {
+      console.log('Promise Executed');
+      console.log(data);
+      var player = '<h2>Lists of Users</h2>';
+      data.forEach(function (user) {
         player += `<div class="player">
                       <div class="strength">Name : ${user.name}</div>
                       <div>Email   : ${user.email}</div>
@@ -34,9 +26,8 @@ function getData () {
       });
       msg.innerHTML = player;
     },
-    (error) => {
-      console.log("Promise rejected");
-      console.log(error.message);
-    }
-  );
+  ).catch((error) => {
+    console.log('Promise rejected');
+    console.log(error.message);
+  })
 };
